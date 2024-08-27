b174df130069d45db6b41882bfdc32c8fc08ce3b
Mohammad Raffiansyah <mohammad.raffiansyah007@gmail.com>
2024-08-27 22:15:35 +0700

    changes AvatarsFallback

--- src/components/layouts/Avatars.tsx
+++ src/components/layouts/Avatars.tsx
@@ -11,7 +11,6 @@ export default function Avatars({ initialUsername, avatarPath }: props) {
   return avatarPath ? (
     <Avatar>
       <AvatarImage src={`${imgUrl}/Avatars/${avatarPath}`} />
-      <AvatarFallback>{initialUsername}</AvatarFallback>
     </Avatar>
   ) : (
     <div className="w-10 h-10 flex rounded-full bg-zinc-900 text-white text-center p-2 items-center justify-center">
